126ee3e444b89d6ba84b6f9d820e30e3c605f2a7
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-03 15:23:28 -0300

    Inicialização do projeto

---
+++ src/main/java/com/duartewallisson/socialnetworkmongo/SocialnetworkmongoApplication.java
@@ -0,0 +1,13 @@
+package com.duartewallisson.socialnetworkmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkmongoApplication.class, args);
+	}
+
+}
