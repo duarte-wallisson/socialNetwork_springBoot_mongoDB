b77f9355d2a78268a3b9d8b97f2261ef70cd187d
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-05 13:00:29 -0300

    Obtenção da Entity User por id e tratamento de requisições

--- src/main/java/com/duartewallisson/socialnetworkmongo/resources/UserResource.java
+++ src/main/java/com/duartewallisson/socialnetworkmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.duartewallisson.socialnetworkmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class UserResource {
         List<UserDTO> listDTO = list.stream().map(it -> new UserDTO(it)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
 }
--- src/main/java/com/duartewallisson/socialnetworkmongo/services/UserService.java
+++ src/main/java/com/duartewallisson/socialnetworkmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.duartewallisson.socialnetworkmongo.services;
 
 import com.duartewallisson.socialnetworkmongo.domain.User;
 import com.duartewallisson.socialnetworkmongo.repository.UserRepository;
+import com.duartewallisson.socialnetworkmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,5 +15,10 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> o = repo.findById(id);
+        return o.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
