e0e9e02bcab7073909a7661800f20292210d326f
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-05 12:01:27 -0300

    Implementação do padrão DTO na Entity User

--- src/main/java/com/duartewallisson/socialnetworkmongo/resources/UserResource.java
+++ src/main/java/com/duartewallisson/socialnetworkmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.duartewallisson.socialnetworkmongo.resources;
 
 import com.duartewallisson.socialnetworkmongo.domain.User;
+import com.duartewallisson.socialnetworkmongo.dto.UserDTO;
 import com.duartewallisson.socialnetworkmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,8 +18,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(it -> new UserDTO(it)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
