4e0bad7b123652f5cbb707cf227e06850a4e57fd
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-21 22:22:46 -0300

    Substituição (Via Query) de pesquisa da Entity Post pelo título

--- src/main/java/com/duartewallisson/socialnetworkmongo/repository/PostRepository.java
+++ src/main/java/com/duartewallisson/socialnetworkmongo/repository/PostRepository.java
@@ -2,9 +2,13 @@ package com.duartewallisson.socialnetworkmongo.repository;
 
 import com.duartewallisson.socialnetworkmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
